7835f850c90e1314c3e018c998d6aa52859eb02c
Kevin <125473461+viizion-03@users.noreply.github.com>
2024-04-06 21:23:55 +0200

    remove home page.js

--- src/routes/(app)/+layout.server.js
+++ src/routes/(app)/+layout.server.js
@@ -1,18 +1,23 @@
-import { UserProfileService } from '$lib/services/backend-services.js';
+import { UserProfileService, VacancyService } from '$lib/services/backend-services.js';
 import { redirect } from '@sveltejs/kit';
 import { loadFlash } from 'sveltekit-flash-message/server';
 // @ts-ignore
 import { Query } from 'appwrite';
-import { createSessionClient } from '$lib/services/appwrite-auth.js';
 
 // @ts-ignore
 export const load = async ({ locals }) => {
-	//get user profile data
+	async function getVacancies() {
+		return new VacancyService().list();
+	}
 
 	const getProfile = async () =>
 		// @ts-ignore
 		new UserProfileService().get(locals.user.$id, [Query.select(['firstname', 'surname'])]);
 
-	// @ts-ignore
-	return { user: locals.user, userProfile: getProfile() };
+	return {
+		// @ts-ignore
+		user: locals.user,
+		userProfile: getProfile(),
+		vacancies: getVacancies()
+	};
 };
