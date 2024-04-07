c4d4d9fb270be5a8159f7b48d66ea55e81206204
Kevin <125473461+viizion-03@users.noreply.github.com>
2024-04-07 18:09:55 +0200

    layout height

--- src/routes/(app)/+layout.svelte
+++ src/routes/(app)/+layout.svelte
@@ -13,63 +13,67 @@
 	$: activeUrl = $page.url.pathname;
 </script>
 
-<Navbar class="white ">
-	<NavBrand href="/">
-		<img src={Logo} class="me-3 h-6 sm:h-9" alt="BERC Logo" />
-	</NavBrand>
+<div class="bg-orange-100 h-svh overflow-y-auto flex flex-col">
+	<Navbar class="white ">
+		<NavBrand href="/">
+			<img src={Logo} class="me-3 h-6 sm:h-9" alt="BERC Logo" />
+		</NavBrand>
 
-	{#if data.user}
-		<div class="flex md:order-2 gap-1">
-			<Button color="none">
-				{data.user.name ? data.user.name : data.user.email}
+		{#if data.user}
+			<div class="flex md:order-2 gap-1">
+				<Button color="none">
+					{data.user.name ? data.user.name : data.user.email}
 
-				<ChevronDownOutline class="size-5 ms-2 text-orange-600 dark:text-white" /></Button
-			>
-			<Dropdown>
-				<div slot="header" class="px-4 py-2">
-					{#await userProfile then p}
-						{#if 'doc' in p}
-							<span class="block text-sm text-gray-900 dark:text-white"
-								>{p?.doc.firstname} {p?.doc.surname}</span
-							>
-							<span class="block truncate text-sm font-medium">{data.user.email}</span>
-						{/if}
-					{/await}
-				</div>
-				<DropdownItem href="/account">Dashboard</DropdownItem>
-				<DropdownItem>Chats</DropdownItem>
-				<DropdownItem>My Organizations</DropdownItem>
-				<DropdownItem href="/account/vacancies">My Vacancies</DropdownItem>
-				<DropdownItem>Vacancy Applications</DropdownItem>
-				<DropdownItem>My Forums</DropdownItem>
+					<ChevronDownOutline class="size-5 ms-2 text-orange-600 dark:text-white" /></Button
+				>
+				<Dropdown>
+					<div slot="header" class="px-4 py-2">
+						{#await userProfile then p}
+							{#if 'doc' in p}
+								<span class="block text-sm text-gray-900 dark:text-white"
+									>{p?.doc.firstname} {p?.doc.surname}</span
+								>
+								<span class="block truncate text-sm font-medium">{data.user.email}</span>
+							{/if}
+						{/await}
+					</div>
+					<DropdownItem href="/account">Dashboard</DropdownItem>
+					<DropdownItem>Chats</DropdownItem>
+					<DropdownItem>My Organizations</DropdownItem>
+					<DropdownItem href="/account/vacancies">My Vacancies</DropdownItem>
+					<DropdownItem>Vacancy Applications</DropdownItem>
+					<DropdownItem>My Forums</DropdownItem>
 
-				<DropdownItem slot="footer">
-					<form action="/account?/logout" method="post">
-						<Button type="submit" class="w-full">Sign Out</Button>
-					</form>
-				</DropdownItem>
-			</Dropdown>
-			<NavHamburger />
-		</div>
-	{:else}
-		<div class="flex md:order-2 gap-1">
-			<div>
-				<a href="/auth/login">
-					<Button size="sm" color="purple">Sign in</Button>
-				</a>
-				<a href="/auth/signup">
-					<Button size="sm" color="light">Get Started</Button>
-				</a>
+					<DropdownItem slot="footer">
+						<form action="/account?/logout" method="post">
+							<Button type="submit" class="w-full">Sign Out</Button>
+						</form>
+					</DropdownItem>
+				</Dropdown>
+				<NavHamburger />
 			</div>
-			<NavHamburger />
-		</div>
-	{/if}
-	<NavUl class="order-1" {activeUrl}>
-		<NavLi href="/">Home</NavLi>
-		<NavLi href="/vacancies">Job vacancies</NavLi>
-		<NavLi href="#">Candidate pool</NavLi>
-		<NavLi href="/about">About</NavLi>
-	</NavUl>
-</Navbar>
+		{:else}
+			<div class="flex md:order-2 gap-1">
+				<div>
+					<a href="/auth/login">
+						<Button size="sm" color="purple">Sign in</Button>
+					</a>
+					<a href="/auth/signup">
+						<Button size="sm" color="light">Get Started</Button>
+					</a>
+				</div>
+				<NavHamburger />
+			</div>
+		{/if}
+		<NavUl class="order-1" {activeUrl}>
+			<NavLi href="/">Home</NavLi>
+			<NavLi href="/vacancies">Job vacancies</NavLi>
+			<NavLi href="#">Candidate pool</NavLi>
+			<NavLi href="/about">About</NavLi>
+		</NavUl>
+	</Navbar>
 
-<slot />
+	<div class="flex-grow">
+		<slot />
+	</div>
+</div>
